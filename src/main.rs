7e7da626c913b9df61d4933e784cc0255f36b4d1
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-06-26 10:36:02 +0100

    Step 1.3

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,22 @@ fn main() {
 
 fn handle_connection(stream: &mut TcpStream) {
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
 
-    let response = "+PONG\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(size) if size != 0 => {
+                let response = "+PONG\r\n";
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            }
+            Ok(_) => {
+                println!("Connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        }
+    }
 }
