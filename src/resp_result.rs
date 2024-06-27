18d8965d87dc7f6112a92c1bcdff34ef6ae84650
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-06-27 13:23:42 +0100

    Step 2.8

--- src/resp_result.rs
+++ src/resp_result.rs
@@ -1,19 +1,26 @@
 use std::fmt;
+use std::num;
 use std::string::FromUtf8Error;
+
+pub type RESPLength = i32;
 
 #[derive(Debug, PartialEq)]
 pub enum RESPError {
     FromUtf8,
+    IncorrectLength(RESPLength),
     OutOfBounds(usize),
-    WrongType,
+    ParseInt,
     Unknown,
+    WrongType,
 }
 
 impl fmt::Display for RESPError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             RESPError::FromUtf8 => write!(f, "Cannot convert from UTF-8"),
+            RESPError::IncorrectLength(length) => write!(f, "Incorrect legth {}", length),
             RESPError::OutOfBounds(index) => write!(f, "Out of bounds at index {}", index),
+            RESPError::ParseInt => write!(f, "Cannot parse string into integer"),
             RESPError::Unknown => write!(f, "Unknown format for RESP string"),
             RESPError::WrongType => write!(f, "Wrong prefix for RESP type"),
         }
@@ -23,6 +30,12 @@ impl fmt::Display for RESPError {
 impl From<FromUtf8Error> for RESPError {
     fn from(_err: FromUtf8Error) -> Self {
         Self::FromUtf8
+    }
+}
+
+impl From<num::ParseIntError> for RESPError {
+    fn from(_err: num::ParseIntError) -> Self {
+        Self::ParseInt
     }
 }
 
