ed5832454d1f511e44ccde53c0ca0b12e40cbdc0
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-08-02 18:26:55 +0100

    Step 5.7.4

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,13 @@
+use crate::resp::bytes_to_resp;
+use crate::server_result::{ServerMessage, ServerValue};
 use crate::{request::Request, server_result::ServerError};
 use std::fmt;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    select,
+    sync::mpsc,
+};
 
 #[derive(Debug)]
 pub enum ConnectionError {
@@ -19,4 +27,87 @@ impl fmt::Display for ConnectionError {
 #[derive(Debug)]
 pub enum ConnectionMessage {
     Request(Request),
+}
+
+pub async fn run_listener(host: String, port: u16, server_sender: mpsc::Sender<ConnectionMessage>) {
+    let listener = TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .unwrap();
+
+    loop {
+        tokio::select! {
+            connection = listener.accept() => {
+                match connection {
+                    Ok((stream, _)) => {
+                        tokio::spawn(handle_connection(stream, server_sender.clone()));
+                    }
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub async fn handle_connection(
+    mut stream: TcpStream,
+    server_sender: mpsc::Sender<ConnectionMessage>,
+) {
+    let mut buffer = [0; 512];
+
+    let (connection_sender, mut connection_receiver) = mpsc::channel::<ServerMessage>(32);
+
+    loop {
+        select! {
+            result = stream.read(&mut buffer) => {
+                match result {
+                    Ok(size) if size != 0 => {
+                        let mut index: usize = 0;
+
+                        let resp = match bytes_to_resp(&buffer[..size].to_vec(), &mut index) {
+                            Ok(v) => v,
+                            Err(e) => {
+                                eprintln!("Error: {}", e);
+                                return;
+                            }
+                        };
+
+                        let request = Request {
+                            value: resp,
+                            sender: connection_sender.clone(),
+                        };
+
+                        match server_sender.send(ConnectionMessage::Request(request)).await {
+                            Ok(()) => {},
+                            Err(e) => {
+                                eprintln!("Error sending request: {}", e);
+                                return;
+                            }
+                        }
+                    }
+                    Ok(_) => {
+                        println!("Connection closed");
+                        break;
+                    }
+                    Err(e) => {
+                        println!("Error: {}", e);
+                        break;
+                    }
+                }
+            }
+
+            Some(response) = connection_receiver.recv() => {
+                let _ = match response {
+                    ServerMessage::Data(ServerValue::RESP(v)) => stream.write_all(v.to_string().as_bytes()).await,
+                    ServerMessage::Error(e) => {
+                        eprintln!("Error: {}", ConnectionError::ServerError(e));
+                        return;
+                    }
+                };
+            }
+
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use crate::connection::ConnectionError;
-use crate::request::Request;
-use crate::resp::{bytes_to_resp, RESP};
-use crate::server_result::ServerValue;
+use crate::resp::RESP;
 use crate::storage::Storage;
-use connection::ConnectionMessage;
+use connection::{run_listener, ConnectionMessage};
 use server::{run_server, Server};
-use server_result::ServerMessage;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-    select,
-    sync::mpsc,
-};
+use tokio::sync::mpsc;
 
 mod connection;
 mod request;
@@ -25,8 +16,6 @@ mod storage_result;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
-
     let storage = Storage::new();
     let mut server = Server::new();
     server = server.set_storage(storage);
@@ -35,77 +24,7 @@ async fn main() -> std::io::Result<()> {
 
     tokio::spawn(run_server(server, server_receiver));
 
-    loop {
-        tokio::select! {
-            connection = listener.accept() => {
-                match connection {
-                    Ok((stream, _)) => {
-                        tokio::spawn(handle_connection(stream, server_sender.clone()));
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                        continue;
-                    }
-                }
-            }
-        }
-    }
-}
+    run_listener("127.0.0.1".to_string(), 6379, server_sender).await;
 
-async fn handle_connection(mut stream: TcpStream, server_sender: mpsc::Sender<ConnectionMessage>) {
-    let mut buffer = [0; 512];
-
-    let (connection_sender, mut connection_receiver) = mpsc::channel::<ServerMessage>(32);
-
-    loop {
-        select! {
-            result = stream.read(&mut buffer) => {
-                match result {
-                    Ok(size) if size != 0 => {
-                        let mut index: usize = 0;
-
-                        let resp = match bytes_to_resp(&buffer[..size].to_vec(), &mut index) {
-                            Ok(v) => v,
-                            Err(e) => {
-                                eprintln!("Error: {}", e);
-                                return;
-                            }
-                        };
-
-                        let request = Request {
-                            value: resp,
-                            sender: connection_sender.clone(),
-                        };
-
-                        match server_sender.send(ConnectionMessage::Request(request)).await {
-                            Ok(()) => {},
-                            Err(e) => {
-                                eprintln!("Error sending request: {}", e);
-                                return;
-                            }
-                        }
-                    }
-                    Ok(_) => {
-                        println!("Connection closed");
-                        break;
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                        break;
-                    }
-                }
-            }
-
-            Some(response) = connection_receiver.recv() => {
-                let _ = match response {
-                    ServerMessage::Data(ServerValue::RESP(v)) => stream.write_all(v.to_string().as_bytes()).await,
-                    ServerMessage::Error(e) => {
-                        eprintln!("Error: {}", ConnectionError::ServerError(e));
-                        return;
-                    }
-                };
-            }
-
-        }
-    }
+    Ok(())
 }
