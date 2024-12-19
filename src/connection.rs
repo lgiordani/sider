284782fab2fa7f27abd9b6378063129f3163d75d
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-12-19 19:00:26 +0000

    Step 5.6

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,20 @@
-use crate::request::Request;
+use crate::{request::Request, server_result::ServerError};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum ConnectionError {
+    ServerError(ServerError),
+}
+
+impl fmt::Display for ConnectionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ConnectionError::ServerError(e) => {
+                write!(f, "{}", format!("Server error: {}", e))
+            }
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum ConnectionMessage {
